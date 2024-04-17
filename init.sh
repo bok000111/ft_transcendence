8c3a944a09fb76b462da544f163897adde75e01d
bok000111 <bok000111@naver.com>
2024-04-17 05:44:57 +0000

    Add init.sh script and update Makefile

---
+++ init.sh
@@ -0,0 +1,11 @@
+echo 'alias manage="python /workspaces/ft_transcendence/srcs/requirements/django/manage.py" >> /home/vscode/.zshrc'
+
+echo 'if [ -z "$SSH_AUTH_SOCK" ]; then
+   # Check for a currently running instance of the agent
+   RUNNING_AGENT="`ps -ax | grep 'ssh-agent -s' | grep -v grep | wc -l | tr -d '[:space:]'`"
+   if [ "$RUNNING_AGENT" = "0" ]; then
+        # Launch a new instance of the agent
+        ssh-agent -s &> $HOME/.ssh/ssh-agent
+   fi
+   eval `cat $HOME/.ssh/ssh-agent`
+fi' >> ~/.zshrc
