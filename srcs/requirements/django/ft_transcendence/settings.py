0bbb25c735d5639a656e7f943df42b5bf65a9abf
bok000111 <bok000111@naver.com>
2024-04-21 05:51:21 +0000

    [FEAT] Add Django user views and admin configuration

--- srcs/requirements/django/ft_transcendence/settings.py
+++ srcs/requirements/django/ft_transcendence/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # TODO: 나중에 환경변수로 변경
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-bhb3@-^i==hy=0@6-i+(i%*#^m9u&r^cus@1s6ih-g88t+eq%c'
+SECRET_KEY = "django-insecure-bhb3@-^i==hy=0@6-i+(i%*#^m9u&r^cus@1s6ih-g88t+eq%c"
 
 # TODO: 배포시 False로 변경
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -36,50 +36,53 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'ft_transcendence',
-    'pong',
-    'daphne',
-    'channels',
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
+    "ft_transcendence",  # 프로젝트 설정
+    "api",  # api 라우팅용
+    "ws",  # websocket 라우팅용
+    "user",  # 유저 관리
+    "pong",  # 게임 관리
+    "daphne",
+    "channels",
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = 'ft_transcendence.urls'
+ROOT_URLCONF = "ft_transcendence.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [
-			os.path.join(BASE_DIR, '../frontend/'),
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [
+            os.path.join(BASE_DIR, "../frontend/"),
         ],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'ft_transcendence.wsgi.application'
-ASGI_APPLICATION = 'ft_transcendence.asgi.application'
+WSGI_APPLICATION = "ft_transcendence.wsgi.application"
+ASGI_APPLICATION = "ft_transcendence.asgi.application"
 
 
 # Database
@@ -87,13 +90,13 @@ ASGI_APPLICATION = 'ft_transcendence.asgi.application'
 
 # TODO: 나중에 환경변수로 변경
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'ft_transcendence',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'localhost',  # PostgreSQL이 로컬에 설치된 경우
-        'PORT': '5432',       # 기본 PostgreSQL 포트
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "ft_transcendence",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": "localhost",  # PostgreSQL이 로컬에 설치된 경우
+        "PORT": "5432",  # 기본 PostgreSQL 포트
     }
 }
 
@@ -103,26 +106,27 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
+AUTH_USER_MODEL = "user.User"
 
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'Asia/Seoul'
+TIME_ZONE = "Asia/Seoul"
 
 USE_I18N = True
 
@@ -132,13 +136,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "static/"
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, '../frontend/'),
+    os.path.join(BASE_DIR, "../frontend/"),
 ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
+DEFAULT_AUTO_FIELD = "django.db.models.AutoField"
