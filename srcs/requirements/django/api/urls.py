205d49e2abe4f351f7389f856056491647feca3f
bok000111 <bok000111@naver.com>
2024-04-22 05:34:17 +0000

    [FEAT] Add include statement and create need_json utility function

--- srcs/requirements/django/api/urls.py
+++ srcs/requirements/django/api/urls.py
@@ -1,8 +1,6 @@
-from django.urls import path
+from django.urls import path, include
 
 from user import views as user_views
-
-# from pong import views as pong_views
 
 urlpatterns = [
     # /api/ TODO: user/views.py 작성
@@ -10,9 +8,5 @@ urlpatterns = [
     path("login/", user_views.login_view, name="login"),
     path("logout/", user_views.logout_view, name="logout"),
     # /api/pong/ TODO: pong/views.py 작성
-    # path("create/", pong_views.create, name="create"),  # 게임 생성
-    # path("join/", pong_views.join, name="join"),  # 게임 참가
-    # path(
-    # "leave/", pong_views.leave, name="leave"
-    # ),  # 게임 나가기 - 새로고침 시 바로 나가지지 않도록 요청으로 분리
+    path("pong/", include("pong.urls")),
 ]
