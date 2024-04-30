ac9ca5ca51b088a65f4ec7a7d3b44ddee98fc4d7
도준웅 <jdoh@dojun-ung-ui-MacBookAir.local>
2024-04-30 09:17:08 +0900

    [MODIFY] 주석 수정

--- srcs/requirements/frontend/js/Page.js
+++ srcs/requirements/frontend/js/Page.js
@@ -4,6 +4,7 @@ import Component from "./Component.js";
 export default class Page extends Component {
     /**
      * 자식 클래스 setup() 메소드에서  mount() 함수를 호출해야 한다.
+     * ex) this.mount("login_page");
      */
     mount(pageName) {
         PageShifter.mount(pageName, this.$elem, this.init.bind(this), this.fini.bind(this));
