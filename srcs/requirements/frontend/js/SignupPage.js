0fc1db61b37cae76f4924194a80c7e3c93279278
도준웅 <jdoh@dojun-ung-ui-MacBookAir.local>
2024-04-30 19:01:52 +0900

    [FEAT] 로그인 페이지, 로그아웃 페이지 분리

--- srcs/requirements/frontend/js/SignupPage.js
+++ srcs/requirements/frontend/js/SignupPage.js
@@ -1,39 +1,31 @@
 import Page from "./Page.js";
 
-/**
- * 성공 시 어떤 정보를 수신하고 저장해야 하는지에 대한 논의 필요.
- * Login 클래스가 그 정보를 저장하는 역할을 한다.
- * 정보를 저장하는 클래스는 Component와 별개로 동작.
- */
-class Login {
-    #username;
-};
-
-class LoginPage extends Page {
+class SignupPage extends Page {
     $form;
     $button;
 
     setup() {
-        this.mount("login_page");
+        this.mount("signup_page");
         this.$form = this.$elem.querySelector("form");
         this.$button = this.$elem.querySelector("button");
     }
 
     setEvent() {
         this.$button.addEventListener("click", () => {
-            this.shift("signup_page");
+            this.shift("login_page");
         });
         this.$form.addEventListener("submit", (event) => {
             event.preventDefault();
-            this.postLoginInfo({
+            this.postSignupInfo({
                 email: this.$form.querySelector("[placeholder=email]").value,
                 password: this.$form.querySelector("[placeholder=password]").value,
+                username: this.$form.querySelector("[placeholder=username]").value,
             });
         });
     }
 
-    postLoginInfo = async (data) => {
-        const response = await fetch("http://localhost:8000/api/login/", {
+    postSignupInfo = async (data) => {
+        const response = await fetch("http://localhost:8000/api/signup/", {
             method: "POST",
             headers: {
                 "Host": "localhost:8000",
@@ -50,12 +42,10 @@ class LoginPage extends Page {
         }
         else {
             const res = await response.json();
-            if (res.success) {
-                this.inputId = res.data.username; // 수정 필요.
-                this.shift("main_page");
-            }
+            if (res.success)
+                this.shift("login_page");
             else
-                alert("Wrong Id or Password!");
+                alert("input is wrong!");
         }
     }
 
@@ -66,5 +56,6 @@ class LoginPage extends Page {
     fini() {
         this.$form.querySelector("[placeholder=email]").value = "";
         this.$form.querySelector("[placeholder=password]").value = "";
+        this.$form.querySelector("[placeholder=username]").value = "";
     }
 };
