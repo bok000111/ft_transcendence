ea7c019a8dd792f197606ac17b6c8398137441b1
jbok <jbok@student.32seoul.kr>
2024-05-10 03:16:03 +0000

    [FEAT] Add user factories and forms for Django user module

---
+++ srcs/requirements/django/user/forms.py
@@ -0,0 +1,44 @@
+from django import forms
+from django.conf import settings
+from django.contrib.auth import get_user_model
+
+
+User = get_user_model()
+
+
+class UserCreateModelForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ["email", "username", "password"]
+        error_messages = {
+            "email": {
+                "unique": "Email already exists",
+                "required": "Email required",
+            },
+            "username": {
+                "unique": "Username already exists",
+                "required": "Username required",
+            },
+            "password": {
+                "required": "Password required",
+            },
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["email"].required = True
+        self.fields["username"].required = True
+        self.fields["password"].required = True
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password"])
+        if commit:
+            user.save()
+
+        return user
+
+
+class UserLoginForm(forms.Form):
+    email = forms.EmailField()
+    password = forms.CharField()
--- srcs/requirements/django/user/views.py
+++ srcs/requirements/django/user/views.py
@@ -1,69 +1,66 @@
-from django.http import JsonResponse
-from django.contrib.auth import aauthenticate, alogin, alogout, get_user_model
-from django.conf import settings
 import json
+from asgiref.sync import sync_to_async
 
-# decorator
-from django.views.decorators.csrf import csrf_exempt
+from django.conf import settings
+from django.contrib.auth import aauthenticate, alogin, alogout, get_user_model
+from django.views import View
 from django.views.decorators.http import require_http_methods
-from api.utils import need_json
-from user.utils import need_auth, need_not_auth
+from channels.db import database_sync_to_async
 
-# model
+from api.utils import need_json, AJsonMixin, JsonResponse
+from user.utils import need_auth
+
+from .forms import UserCreateModelForm, UserLoginForm
+
 User = get_user_model()
 
 
-@csrf_exempt
-@require_http_methods(["POST"])
-@need_json
-async def signup_view(request):
-    data = json.loads(request.body)
-    email = data.get("email", None)
-    username = data.get("username", None)
-    password = data.get("password", None)
-
-    try:
-        user = await User.objects.create_user(
-            email=email, username=username, password=password
-        )
-        return JsonResponse({"message": "User created."}, status=201)
-    except ValueError as e:
-        return JsonResponse({"message": f"{e}"}, status=400)
-    except Exception as e:
-        if settings.DEBUG:
-            return JsonResponse({"message": f"{e}"}, status=500)
-        return JsonResponse({"message": "Something went wrong."}, status=500)
+class SignUpView(AJsonMixin, View):
+    async def post(self, request):
+        form = UserCreateModelForm(request.json)
+        if await sync_to_async(form.is_valid)():
+            new_user = await database_sync_to_async(form.save)()
+            return await self.ajsend_created(
+                {
+                    "message": "User created",
+                    "data": {"user": new_user},
+                }
+            )
+        else:
+            return self.jsend_bad_request(
+                {
+                    "message": "Invalid data",
+                    "data": form.errors.as_json(),
+                }
+            )
 
 
-@csrf_exempt
-@require_http_methods(["POST"])
-@need_not_auth
-@need_json
-async def login_view(request):
-    data = json.loads(request.body)
-    email = data.get("email", None)
-    password = data.get("password", None)
-
-    if not email or not password:
-        return JsonResponse({"message": "Invalid request."}, status=400)
-
-    user = await aauthenticate(request, email=email, password=password)
-    if user is None:
-        return JsonResponse({"message": "Invalid credentials."}, status=400)
-
-    await alogin(request, user)
-
-    return JsonResponse({"message": "Logged in."}, status=200)
+class LoginView(AJsonMixin, View):
+    async def post(self, request):
+        if (await request.auser()).is_authenticated:
+            return self.jsend_bad_request({"message": "Already logged in"})
+        form = UserLoginForm(request.json)
+        if await sync_to_async(form.is_valid)():
+            user = await aauthenticate(
+                request,
+                email=form.cleaned_data["email"],
+                password=form.cleaned_data["password"],
+            )
+            if user is not None:
+                await alogin(request, user)
+                return await self.ajsend_ok(
+                    {"message": "Logged in", "data": {"user": user}}
+                )
+        return self.jsend_bad_request({"message": "Invalid credentials"})
 
 
-@csrf_exempt
 @require_http_methods(["POST"])
 @need_auth
 @need_json
 async def logout_view(request):
     await alogout(request)
 
-    return JsonResponse({"message": "Logged out."}, status=200)
+    return JsonResponse({"message": "Logged out"}, status=200)
 
 
 # Path: srcs/requirements/django/user/models.py
