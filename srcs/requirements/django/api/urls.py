a5e9ed5f36cd8b36bcc63a484972a10d0d62b792
jbok <jbok@student.32seoul.kr>
2024-05-10 03:16:14 +0000

    [FEAT] Update user views to use class-based views for signup and login

--- srcs/requirements/django/api/urls.py
+++ srcs/requirements/django/api/urls.py
@@ -3,10 +3,9 @@ from django.urls import path, include
 from user import views as user_views
 
 urlpatterns = [
-    # /api/ TODO: user/views.py 작성
-    path("signup/", user_views.signup_view, name="signup"),
-    path("login/", user_views.login_view, name="login"),
+    path("signup/", user_views.SignUpView.as_view(), name="signup"),
+    path("login/", user_views.LoginView.as_view(), name="login"),
     path("logout/", user_views.logout_view, name="logout"),
-    # /api/pong/ TODO: pong/views.py 작성
-    path("pong/", include("pong.urls")),
+    # path("pong/", include("pong.urls")),
+    path("lobby/", include("lobby.urls")),
 ]
