8ddcb3345871a176553be1650888ce684878cfd9
도준웅 <jdoh@dojun-ung-ui-MacBookAir.local>
2024-05-23 10:13:44 +0900

    [FEAT] Info class added

---
+++ srcs/requirements/frontend/js/models/Info.js
@@ -0,0 +1,9 @@
+const Info = {
+    username,       // string
+    lobby: {
+        lobbyID,    // number
+        nickname,   // string
+        isHost,     // boolean
+        isReady     // boolean
+    }
+};
