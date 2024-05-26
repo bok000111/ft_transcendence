2a8699b96030e90a12ba5ebd1607e010266b121b
jbok <jbok@student.32seoul.kr>
2024-05-26 06:05:32 +0000

    [FEAT] chore: Update Django URLs for user-related views

---
+++ srcs/requirements/django/user/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from user import views
+
+urlpatterns = [
+    path("signup/", views.SignUpView.as_view(), name="signup"),
+    path("login/", views.LoginView.as_view(), name="login"),
+    path("logout/", views.LogoutView.as_view(), name="logout"),
+    path("me/", views.MyInfoView.as_view(), name="myinfo"),
+]
