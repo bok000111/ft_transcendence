8b7fd1f574085daf0cff1af93244dfa5eff10446
jbok <jbok@student.32seoul.kr>
2024-05-26 06:05:55 +0000

    Refactor max_players factory attribute to generate values between 2 and 4

--- srcs/requirements/django/lobby/factories.py
+++ srcs/requirements/django/lobby/factories.py
@@ -7,5 +7,5 @@ class LobbyPostFactory(factory.Factory):
         model = dict
 
     name = factory.LazyAttribute(lambda _: f"{faker.Faker().word()}")
-    max_players = factory.Faker("random_int", min=3, max=4)  # temp 3 (full room error)
+    max_players = factory.Faker("random_int", min=2, max=4)
     end_score = factory.Faker("random_int", min=1, max=10)
