1abfcb828f205da83f1b614feba8f167807f4ead
jbok <jbok@student.32seoul.kr>
2024-05-26 06:05:32 +0000

    [FEAT] chore: Update Django URLs for user-related views

--- srcs/requirements/django/api/urls.py
+++ srcs/requirements/django/api/urls.py
@@ -1,11 +1,8 @@
 from django.urls import path, include
 
-from user import views as user_views
 
 urlpatterns = [
-    path("signup/", user_views.SignUpView.as_view(), name="signup"),
-    path("login/", user_views.LoginView.as_view(), name="login"),
-    path("logout/", user_views.logout_view, name="logout"),
-    # path("pong/", include("pong.urls")),
+    path("user/", include("user.urls")),
     path("lobby/", include("lobby.urls")),
+    # path("pong/", include("pong.urls")),
 ]
