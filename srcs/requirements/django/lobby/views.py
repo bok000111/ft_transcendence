152ed32f21ad1ceba7c33e1cb08ec110d4b8d23b
jbok <jbok@student.32seoul.kr>
2024-05-26 10:56:38 +0000

    chore: Refactor Django lobby views to use async/await syntax

--- srcs/requirements/django/lobby/views.py
+++ srcs/requirements/django/lobby/views.py
@@ -1,20 +1,8 @@
-import json
-from django.db import IntegrityError
-from django.core import serializers
-from django.core.serializers.json import DjangoJSONEncoder
 from django.views.generic import View
 
-# from django.http import JsonResponse
-
-# from django.db.utils import IntegrityError
-# from django.utils.decorators import method_decorator
 from channels.db import database_sync_to_async
 
-from asgiref.sync import sync_to_async
-
-
-from user.utils import need_auth
-from api.utils import need_json, AJsonMixin, AJsonAuthRequiredMixin
+from api.utils import AJsonMixin, AJsonAuthRequiredMixin
 
 from .models import GameLobby, PlayerInLobby
 from .forms import LobbyCreateModelForm, NicknameForm
