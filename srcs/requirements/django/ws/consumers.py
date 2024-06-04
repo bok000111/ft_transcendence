c82eab952e193bead6fc89ba42dbf7a4ff3bf842
jbok <jbok@student.32seoul.kr>
2024-06-04 04:38:20 +0000

    [FEAT] Refactor lobby and game classes for WebSocket implementation

--- srcs/requirements/django/ws/consumers.py
+++ srcs/requirements/django/ws/consumers.py
@@ -24,3 +24,51 @@ class WSConsumer(AsyncWebsocketConsumer):
         match action:
             case "":
                 pass
+
+    #
+
+
+class Lobby:
+    async def start(self):
+        # 대충 게임 만들어서 시작하고 게임 객체 반환
+        pass
+
+    async def set_nick(self, user, nick: str):
+        # 대충 닉네임 설정
+        pass
+
+    async def ready(self, user):
+        # 대충 유저가 준비했다고 처리
+        pass
+
+    async def config(self, user, config):
+        # 대충 유저가 설정을 보냈을 때 처리
+        pass
+
+
+class Game:
+    def __init__(self) -> None:
+        # 대충 게임 초기화
+        pass
+
+    async def start(self):
+        # 대충 게임 시작
+        pass
+
+    async def input(self):
+        # 대충 입력 받아서 처리
+        pass
+
+
+class Tournament:
+    def __init__(self) -> None:
+        # 매칭된 유저들 받아서 토너먼트 시작
+        pass
+
+    async def start(self):
+        # 대충 토너먼트 시작 하위 2개의 게임 객체 생성하고 시작
+        pass
+
+    async def finish_match(self, winner):
+        # 대충 내부 경기 끝났을 때 처리 이긴사람 받아서 처리
+        pass
