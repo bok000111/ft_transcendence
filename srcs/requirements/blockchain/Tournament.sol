291de55033e005b134d0cfddc8873675b0877b47
Sunwoo Song <sunwsong@c1r5s7.42seoul.kr>
2024-06-06 20:42:04 +0900

    [TEMP] 블록체인 구현 중

--- srcs/requirements/blockchain/Tournament.sol
+++ srcs/requirements/blockchain/Tournament.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.6.0 <0.9.0;
+pragma solidity ^0.6.0;
 
 contract Tournament {
 
@@ -12,6 +12,9 @@ contract Tournament {
     }
 
     struct Round {
+        Player[2] players;
+        string round_type;
+        uint64 winner_id;
         uint8[2] score;
     }
 
@@ -20,22 +23,27 @@ contract Tournament {
 
     mapping(uint64 => Player) private players;
 
-    function addPlayer(uint64 _id, string memory _username, string memory _nickname) external {
+    function addPlayer(uint64 _id, string calldata _username, string calldata _nickname) external {
         players[_id] = Player(_id, _username, _nickname);
     }
 
     function saveResult() private {
-        finished_round = 0;
-        
+        // round의 형식에 맞추어 result를 저장
     }
 
-    function setWinner(uint8 round, uint8[2] memory score) external {
+    function setWinner(uint8 round, uint8[2] calldata score) external returns(uint64) {
         rounds[round].score = score;
         finished_round += 1;
 
         if (finished_round == ROUND_COUNT)
             saveResult();
+        
+        return 1;
     }
 
+    function setFinishedRound(uint8 _finished_round) external returns(uint64){
+        finished_round = _finished_round;
+        return finished_round;
+    }
 
 }
--- srcs/requirements/blockchain/deploy.py
+++ srcs/requirements/blockchain/deploy.py
@@ -1,16 +1,15 @@
-import os
-from solcx import compile_standard, install_solc
 import json
 from web3 import Web3
+from solcx import compile_standard, install_solc
+import os
 from dotenv import load_dotenv
 
 load_dotenv()
-install_solc("0.8.0"),
+install_solc("0.6.0"),
 with open("./Tournament.sol", "r") as file:
     tournament_file = file.read()
 
 # Compile Our Solidity
-
 compiled_sol = compile_standard(
     {
         "language": "Solidity",
@@ -23,7 +22,7 @@ compiled_sol = compile_standard(
             }
         },
     },
-    solc_version="0.8.0",
+    solc_version="0.6.0",
 )
 
 with open("compiled_code.json", "w") as file:
@@ -38,27 +37,45 @@ bytecode = compiled_sol["contracts"]["Tournament.sol"]["Tournament"]["evm"][
 abi = compiled_sol["contracts"]["Tournament.sol"]["Tournament"]["abi"]
 
 # For connecting to Ganache
-w3 = Web3(Web3.HTTPProvider("http://127.0.0.1:7545"))
+w3 = Web3(Web3.HTTPProvider("http://127.0.0.1:8545"))
 chain_id = 1337
 my_address = os.getenv("ADDRESS")
 private_key = os.getenv("PRIVATE_KEY")
-print(private_key)
+print(w3)
 
 Tournament = w3.eth.contract(abi=abi, bytecode=bytecode)
 print(Tournament)
 
 # Get the latest transaction
 nonce = w3.eth.get_transaction_count(my_address)
-print(nonce)
 
 # 1. Build a transaction
 # 2. Sign a transaction
 # 3. Send a transaction
-transaction = Tournament.constructor().buildTransaction(
-    {"chainId": chain_id, "from": my_address, "nonce": nonce}
+transaction = Tournament.constructor().build_transaction(
+    {
+        "chainId": chain_id,
+        "gasPrice": w3.eth.gas_price,
+        "from": my_address,
+        "nonce": nonce,
+    }
 )
 print(transaction)
 
 signed_txn = w3.eth.account.sign_transaction(
     transaction, private_key=private_key)
+
 tx_hash = w3.eth.send_raw_transaction(signed_txn.rawTransaction)
+tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
+
+tournament = w3.eth.contract(address=tx_receipt.contractAddress, abi=abi)
+
+store_transaction = tournament.functions.setFinishedRound(10).build_transaction(
+    {"chainId": chain_id, "from": my_address,
+        "nonce": nonce + 1, "gasPrice": w3.eth.gas_price}
+)
+signed_store_txn = w3.eth.account.sign_transaction(
+    store_transaction, private_key=private_key
+)
+send_store_tx = w3.eth.send_raw_transaction(signed_store_txn.rawTransaction)
+tx_receipt = w3.eth.wait_for_transaction_receipt(send_store_tx)
