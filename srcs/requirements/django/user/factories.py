15ffc8c38d6af45289cd6614f5b505ec7c4187c0
jbok <jbok@student.32seoul.kr>
2024-06-06 08:16:36 +0000

    [CHORE] Update user factories to use unique email and username functions

--- srcs/requirements/django/user/factories.py
+++ srcs/requirements/django/user/factories.py
@@ -1,25 +1,24 @@
 from django.contrib.auth import get_user_model
 import factory
-import faker
+from faker import Faker
 
 User = get_user_model()
+faker = Faker()
 
 
 class UserFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = User
 
-    email = factory.LazyAttribute(lambda _: f"{faker.Faker().email()}")
-    username = factory.LazyAttribute(lambda _: f"{faker.Faker().user_name()}")
-    password = factory.PostGenerationMethodCall(
-        "set_password", faker.Faker().password()
-    )
+    email = factory.LazyFunction(faker.unique.email)
+    username = factory.LazyFunction(faker.unique.user_name)
+    password = factory.PostGenerationMethodCall("set_password", faker.password())
 
 
 class SignUpFactory(factory.Factory):
     class Meta:
         model = dict
 
-    email = factory.LazyAttribute(lambda _: f"{faker.Faker().email()}")
-    username = factory.LazyAttribute(lambda _: f"{faker.Faker().user_name()}")
+    email = factory.LazyFunction(faker.unique.email)
+    username = factory.LazyFunction(faker.unique.user_name)
     password = factory.Faker("password")
