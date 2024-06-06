d17e4b0a90959c6a0e3c90568d129f6021046278
jbok <jbok@student.32seoul.kr>
2024-06-06 10:55:02 +0000

    [HOTFIX] remove async function in api view

--- srcs/requirements/django/api/utils.py
+++ srcs/requirements/django/api/utils.py
@@ -34,42 +34,23 @@ class JsonResponse(JsonResponse):
 
 
 def need_json(view):
-    if iscoroutinefunction(view):
+    @wraps(view)
+    def _wrapped_view(request, *args, **kwargs):
+        if request.content_type != "application/json":
+            return JsonResponse(
+                {
+                    "status": "fail",
+                    "data": {"content_type": "invalid content type"},
+                    "message": "invalid content type",
+                },
+                status=400,
+            )
+        return view(request, *args, **kwargs)
 
-        @wraps(view)
-        async def _wrapped_view(request, *args, **kwargs):
-            if request.content_type != "application/json":
-                return JsonResponse(
-                    {
-                        "status": "fail",
-                        "data": {"content_type": "invalid content type"},
-                        "message": "invalid content type",
-                    },
-                    status=400,
-                )
-            return await view(request, *args, **kwargs)
-
-        return _wrapped_view
-
-    else:
-
-        @wraps(view)
-        def _wrapped_view(request, *args, **kwargs):
-            if request.content_type != "application/json":
-                return JsonResponse(
-                    {
-                        "status": "fail",
-                        "data": {"content_type": "invalid content type"},
-                        "message": "invalid content type",
-                    },
-                    status=400,
-                )
-            return view(request, *args, **kwargs)
-
-        return _wrapped_view
+    return _wrapped_view
 
 
-class AJsonAuthRequiredMixin(AccessMixin):
+class JsonAuthRequiredMixin(AccessMixin):
     def handle_no_permission(self):
         return JsonResponse(
             {
@@ -80,14 +61,14 @@ class AJsonAuthRequiredMixin(AccessMixin):
             status=401,
         )
 
-    async def dispatch(self, request, *args, **kwargs):
-        if not (await request.auser()).is_authenticated:
+    def dispatch(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:
             return self.handle_no_permission()
-        return await super().dispatch(request, *args, **kwargs)
+        return super().dispatch(request, *args, **kwargs)
 
 
-class AJsonMixin:
-    async def dispatch(self, request, *args, **kwargs):
+class JsonMixin:
+    def dispatch(self, request, *args, **kwargs):
         match request.method:
             case "POST" | "PUT" | "PATCH":
                 if request.content_type != "application/json":
@@ -100,7 +81,7 @@ class AJsonMixin:
                     return self.jsend_bad_request(
                         {"content": "invalid json"}, "invalid json"
                     )
-        return await super().dispatch(request, *args, **kwargs)
+        return super().dispatch(request, *args, **kwargs)
 
     def json_response(
         self, status="success", data=None, message=None, code=200, **kwargs
@@ -109,57 +90,25 @@ class AJsonMixin:
             {"status": status, "data": data, "message": message}, status=code, **kwargs
         )
 
-    @database_sync_to_async
-    def ajson_response(self, status, data, message, code, **kwargs):
-        return self.json_response(status, data, message, code, **kwargs)
-
     def jsend_ok(self, data, message, **kwargs):
         return self.json_response("success", data, message, 200, **kwargs)
-
-    @database_sync_to_async
-    def ajsend_ok(self, data, message, **kwargs):
-        return self.jsend_ok(data, message, **kwargs)
 
     def jsend_created(self, data, message, **kwargs):
         return self.json_response("success", data, message, 201, **kwargs)
 
-    @database_sync_to_async
-    def ajsend_created(self, data, message, **kwargs):
-        return self.jsend_created(data, message, **kwargs)
-
     def jsend_bad_request(self, data, message, **kwargs):
         return self.json_response("fail", data, message, 400, **kwargs)
-
-    @database_sync_to_async
-    def ajsend_bad_request(self, data, message, **kwargs):
-        return self.jsend_bad_request(data, message, **kwargs)
 
     def jsend_unauthorized(self):
         return self.json_response(
             "fail", {"auth": "authorization required"}, "authorization required", 401
         )
 
-    @database_sync_to_async
-    def ajsend_unauthorized(self):
-        return self.jsend_unauthorized()
-
     def jsend_forbidden(self):
         return self.json_response("fail", {"auth": "forbidden"}, "forbidden", 403)
-
-    @database_sync_to_async
-    def ajsend_forbidden(self):
-        return self.jsend_forbidden()
 
     def jsend_not_found(self, data, message, **kwargs):
         return self.json_response("fail", data, message, 404, **kwargs)
 
-    @database_sync_to_async
-    def ajsend_not_found(self, data, message, **kwargs):
-        return self.jsend_not_found(data, message, **kwargs)
-
     def jsend_internal_error(self):
         return self.json_response("error", None, "internal server error", 500)
-
-    @database_sync_to_async
-    def ajsend_internal_error(self):
-        return self.jsend_internal_error()
--- srcs/requirements/django/user/utils.py
+++ srcs/requirements/django/user/utils.py
@@ -4,72 +4,34 @@ from inspect import iscoroutinefunction
 
 
 def need_auth(view):
-    if iscoroutinefunction(view):
+    @wraps(view)
+    def _wrapped_view(request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            return JsonResponse(
+                {
+                    "status": "fail",
+                    "data": {"auth": "authorization required"},
+                    "message": "authorization required",
+                },
+                status=401,
+            )
+        return view(request, *args, **kwargs)
 
-        @wraps(view)
-        async def _wrapped_view(request, *args, **kwargs):
-            if not (await request.auser()).is_authenticated:
-                return JsonResponse(
-                    {
-                        "status": "fail",
-                        "data": {"auth": "authorization required"},
-                        "message": "authorization required",
-                    },
-                    status=401,
-                )
-            return await view(request, *args, **kwargs)
-
-        return _wrapped_view
-
-    else:
-
-        @wraps(view)
-        def _wrapped_view(request, *args, **kwargs):
-            if not request.user.is_authenticated:
-                return JsonResponse(
-                    {
-                        "status": "fail",
-                        "data": {"auth": "authorization required"},
-                        "message": "authorization required",
-                    },
-                    status=401,
-                )
-            return view(request, *args, **kwargs)
-
-        return _wrapped_view
+    return _wrapped_view
 
 
 def need_not_auth(view):
-    if iscoroutinefunction(view):
+    @wraps(view)
+    def _wrapped_view(request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return JsonResponse(
+                {
+                    "status": "fail",
+                    "data": {"auth": "already logged in"},
+                    "message": "already logged in",
+                },
+                status=403,
+            )
+        return view(request, *args, **kwargs)
 
-        @wraps(view)
-        async def _wrapped_view(request, *args, **kwargs):
-            if (await request.auser()).is_authenticated:
-                return JsonResponse(
-                    {
-                        "status": "fail",
-                        "data": {"auth": "already logged in"},
-                        "message": "already logged in",
-                    },
-                    status=403,
-                )
-            return await view(request, *args, **kwargs)
-
-        return _wrapped_view
-
-    else:
-
-        @wraps(view)
-        def _wrapped_view(request, *args, **kwargs):
-            if request.user.is_authenticated:
-                return JsonResponse(
-                    {
-                        "status": "fail",
-                        "data": {"auth": "already logged in"},
-                        "message": "already logged in",
-                    },
-                    status=403,
-                )
-            return view(request, *args, **kwargs)
-
-        return _wrapped_view
+    return _wrapped_view
