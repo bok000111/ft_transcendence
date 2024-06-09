68b8d8a55fb59551f73aef84d7525b2b256d32ac
RibbonDragon <gbe0808>
2024-06-09 15:22:28 +0900

    [FEAT] result parsing finished

---
+++ srcs/requirements/blockchain/TournamentResult.py
@@ -0,0 +1,54 @@
+from datetime import datetime
+
+class TournamentResult:
+    def __init__(self, raw_data):
+        split_data = raw_data.split(',')
+        self.timestamp = datetime.fromtimestamp(int(split_data[0]))
+        player_ids = [int(id) for id in split_data[1:5]]
+
+        sub_games_data = split_data[5:]
+        self.sub_games = []
+        for i in range(0, len(sub_games_data), 3):
+            game_id = int(sub_games_data[i])
+            score = [int(sub_games_data[i+1]), int(sub_games_data[i+2])]
+            if game_id == 2:
+                players = player_ids[:2]  # player1, player2
+                game_type = 'semi_final'
+            elif game_id == 3:
+                players = player_ids[2:]  # player3, player4
+                game_type = 'semi_final'
+            elif game_id == 1:
+                # ID가 1인 게임은 나중에 승자가 결정된 후 처리
+                continue
+            sub_game = SubGame(players, game_id, game_type, score)
+            self.sub_games.append(sub_game)
+        
+        final_game_data = sub_games_data[:3]
+        final_game_id = int(final_game_data[0])
+        final_score = [int(final_game_data[1]), int(final_game_data[2])]
+        semi_final_winners = [self.sub_games[0].winner_id, self.sub_games[1].winner_id]
+        final_game = SubGame(semi_final_winners, final_game_id, 'final', final_score)
+        self.sub_games.append(final_game)
+
+    def __str__(self):
+        return f"Timestamp: {self.timestamp}, Sub Games[0]: {self.sub_games[0]}, Sub Games[1]: {self.sub_games[1]}, Sub Games[2]: {self.sub_games[2]}"
+
+    def __repr__(self):
+        return f"Timestamp: {self.timestamp}, Sub Games: {self.sub_games}"
+
+class SubGame:
+    def __init__(self, players, game_id, game_type, score):
+        self.players = players
+        game_id = game_id
+        self.game_type = game_type
+        self.score = score
+        self.winner_id = self.determine_winner()
+
+    def determine_winner(self):
+        if self.score[0] > self.score[1]:
+            return self.players[0]
+        else:
+            return self.players[1]
+
+    def __str__(self):
+        return f"Players: {self.players}, Game Type: {self.game_type}, Score: {self.score}, Winner: {self.winner_id}"
--- srcs/requirements/blockchain/deploy.py
+++ srcs/requirements/blockchain/deploy.py
@@ -3,20 +3,23 @@ from web3 import Web3
 from solcx import compile_standard, install_solc
 import os
 from dotenv import load_dotenv
+from TournamentResult import TournamentResult
 
-# 1. 초기에 Smart contract를 배포하는 함수(1번만 하면 됨)
-# 2. 받은 결과를 바탕으로 contract 객체를 배포하는 함수(tournament가 끝나는 시점마다 실행)
-# 3. 현재 있는 contract 객체를 바탕으로 저장된 결과들을 가공해서 frontend에 던지는 함수
-class TournamentContract:
+class TournamentResultManager:
     def __init__(self, sol_path, provider):
         self.solc_version = "0.6.0"
         self.w3 = Web3(Web3.HTTPProvider(provider))
-        self.chain_id = 1337
-        self.chain_address = os.getenv("ADDRESS")
+        self.chain_address = os.getenv("CHAIN_ADDRESS")
         self.private_key = os.getenv("PRIVATE_KEY")
-        self.bytecode = None
-        self.abi = None
-        self.__compile_sol(sol_path)
+        self.abi = os.getenv("ABI")
+        # self.__compile_sol(sol_path)
+        self.chain_id = 1337
+        receipt = self.__deploy_contract(sol_path)
+        if receipt == None:
+            self.contract_address = os.getenv("CONTRACT_ADDRESS")
+        else:
+            self.contract_address = str(receipt.contractAddress)
+
 
     def __compile_sol(self, sol_path):
         install_solc(self.solc_version)
@@ -25,7 +28,7 @@ class TournamentContract:
         compiled_sol = compile_standard(
             {
                 "language": "Solidity",
-                "sources": {"TournamentManager.sol": {"content": tournament_file}},
+                "sources": {"TournamentContract.sol": {"content": tournament_file}},
                 "settings": {
                     "outputSelection": {
                         "*": {
@@ -36,19 +39,20 @@ class TournamentContract:
             },
             solc_version=self.solc_version,
         )
-        self.bytecode = compiled_sol["contracts"]["TournamentManager.sol"]["TournamentManager"]["evm"]["bytecode"]["object"]
-        # for get bytecode
-        with open("bytecode.json", "w") as file:
-            json.dump(self.bytecode, file)
-        self.abi = compiled_sol["contracts"]["TournamentManager.sol"]["TournamentManager"]["abi"]
-        
+        bytecode = compiled_sol["contracts"]["TournamentContract.sol"]["TournamentContract"]["evm"]["bytecode"]["object"]
+        self.abi = compiled_sol["contracts"]["TournamentContract.sol"]["TournamentContract"]["abi"]
         # for get abi
-        # with open("abi.json", "w") as file:
-        #     json.dump(self.abi, file)
+        with open("abi.json", "w") as file:
+            json.dump(self.abi, file)
+        return bytecode
 
-    def deploy_contract(self):
-        Tournament = self.w3.eth.contract(abi=self.abi, bytecode=self.bytecode)
+    def __deploy_contract(self, sol_path):
         nonce = self.w3.eth.get_transaction_count(self.chain_address)
+        if nonce != 0:
+            print("Contract already deployed")
+            return None
+        bytecode = self.__compile_sol(sol_path)
+        Tournament = self.w3.eth.contract(abi=self.abi, bytecode=bytecode)
         transaction = Tournament.constructor().build_transaction(
             {
                 "chainId": self.chain_id,
@@ -59,12 +63,13 @@ class TournamentContract:
         )
         signed_txn = self.w3.eth.account.sign_transaction(transaction, private_key=self.private_key)
         tx_hash = self.w3.eth.send_raw_transaction(signed_txn.rawTransaction)
-        self.tx_receipt = self.w3.eth.wait_for_transaction_receipt(tx_hash)
-        return self.tx_receipt.contractAddress
+        tx_receipt = self.w3.eth.wait_for_transaction_receipt(tx_hash)
+        print(tx_receipt.contractAddress)
+        return tx_receipt.contractAddress
 
     def start_game(self, id, timestamp, players):
         nonce = self.w3.eth.get_transaction_count(self.chain_address)
-        started_game = self.w3.eth.contract(address=self.tx_receipt.contractAddress, abi=self.abi)
+        started_game = self.w3.eth.contract(address=self.contract_address, abi=self.abi)
         tx = started_game.functions.add_game(id, timestamp, players).build_transaction(
             {
                 "chainId": self.chain_id,
@@ -81,13 +86,13 @@ class TournamentContract:
     
     def save_sub_game(self, id, sub_game_info):
         nonce = self.w3.eth.get_transaction_count(self.chain_address)
-        sub_game = self.w3.eth.contract(address=self.tx_receipt.contractAddress, abi=self.abi)
+        sub_game = self.w3.eth.contract(address=self.contract_address, abi=self.abi)
         tx = sub_game.functions.add_sub_game(id, sub_game_info).build_transaction(
             {
                 "chainId": self.chain_id,
                 "gasPrice": self.w3.eth.gas_price,
                 "from": self.chain_address,
-                "nonce": nonce,
+                "nonce": nonce
             }
         )
         signed_txn = self.w3.eth.account.sign_transaction(tx, private_key=self.private_key)
@@ -97,9 +102,8 @@ class TournamentContract:
         return tx_receipt
     
     def get_all_tournaments(self):
-        cont = self.w3.eth.contract(address=self.tx_receipt.contractAddress, abi=self.abi)
+        cont = self.w3.eth.contract(address=self.contract_address, abi=self.abi)
         valid_tournaments = cont.functions.get_valid_tournaments().call()
-        print(valid_tournaments)
         all_tournaments = []
         for id in valid_tournaments:
             tournament_info = cont.functions.get_tournament(id).call()
@@ -107,14 +111,15 @@ class TournamentContract:
         return all_tournaments
 
 # Usage
-load_dotenv()
-tournament_contract = TournamentContract("./TournamentManager.sol", os.getenv("GANACHE_URL"))
-contract_address = tournament_contract.deploy_contract()
-print(f"Contract deployed at address: {contract_address}")
-tournament_contract.start_game(12, 1625140800, [123, 456, 789, 1011])
-tournament_contract.save_sub_game(12, [2, 123, 456, 1, 10])
-tournament_contract.save_sub_game(12, [3, 789, 1011, 10, 5])
-tx_receipt = tournament_contract.save_sub_game(12, [1, 456, 789, 3, 10])
+load_dotenv() 
+tournament_contract = TournamentResultManager("./TournamentContract.sol", os.getenv("GANACHE_URL"))
+tournament_contract.start_game(20, 1625940800, [263, 456, 989, 1011])
+tournament_contract.save_sub_game(20, [2, 10, 1])
+tournament_contract.save_sub_game(20, [3, 2, 10])
+tournament_contract.save_sub_game(20, [1, 4, 10])
+
 a = tournament_contract.get_all_tournaments()
 print(a)
 
+for res in a:
+    print(TournamentResult(res))
