b7deeedf275a043334f0006d3a51774532f7acd2
mingkang <mingkang@student.42seoul.kr>
2024-06-13 11:36:45 +0000

    게임 임시 구현

---
+++ srcs/requirements/django/ws/ball.py
@@ -0,0 +1,44 @@
+from .constants import *
+
+
+class Ball:
+    def __init__(self):
+        self.pos = {"x": SCREEN_WIDTH / 2, "y": SCREEN_HEIGHT / 2}
+        self.vel = {"x": DEFAULT_SPEED, "y": DEFAULT_SPEED}
+        self.speed_increment = 1  # 속도가 증가하는 양
+        self.max_speed = 10  # 볼의 최대 속도
+
+    def move(self):
+        self.pos["x"] += self.vel["x"]
+        self.pos["y"] += self.vel["y"]
+
+    def bounce(self, direction):
+        if direction == "x":
+            self.vel["x"] *= -1
+        elif direction == "y":
+            self.vel["y"] *= -1
+
+    def increase_speed(self):
+        # x와 y 방향으로 속도를 증가시킨다. 단, 최대 속도를 초과하지 않도록 한다.
+        if (
+            self.vel["x"] > 0
+            and self.vel["x"] < self.max_speed
+            or self.vel["x"] < 0
+            and self.vel["x"] > -self.max_speed
+        ):
+            self.vel["x"] += (
+                self.speed_increment if self.vel["x"] > 0 else -self.speed_increment
+            )
+        if (
+            self.vel["y"] > 0
+            and self.vel["y"] < self.max_speed
+            or self.vel["y"] < 0
+            and self.vel["y"] > -self.max_speed
+        ):
+            self.vel["y"] += (
+                self.speed_increment if self.vel["y"] > 0 else -self.speed_increment
+            )
+
+    def reset_pos(self):
+        self.pos = {"x": SCREEN_WIDTH / 2, "y": SCREEN_HEIGHT / 2}
+        self.vel = {"x": DEFAULT_SPEED, "y": DEFAULT_SPEED}
---
+++ srcs/requirements/django/ws/player.py
@@ -0,0 +1,61 @@
+from .constants import *
+
+
+class Player:
+    def __init__(self, idx, nickname):
+        self.idx = idx
+        self.nickname = nickname
+        self.pos = self.initial_position()
+        self.score = 0
+        self.up = False
+        self.down = False
+
+    def initial_position(self):
+        if self.idx == 0:
+            return {"x": PADDLE_WIDTH, "y": SCREEN_HEIGHT / 2}
+        elif self.idx == 1:
+            return {"x": SCREEN_WIDTH - PADDLE_WIDTH, "y": SCREEN_HEIGHT / 2}
+        elif self.idx == 2:
+            return {"x": SCREEN_WIDTH / 2, "y": PADDLE_WIDTH}
+        elif self.idx == 3:
+            return {"x": SCREEN_WIDTH / 2, "y": SCREEN_HEIGHT - PADDLE_WIDTH}
+
+    # keyevent -> {
+    # 1 : x방향 + 키가 눌림.
+    # 2 : x방향 - 키가 눌림.
+    # 3 : x방향 + 키 떼짐.
+    # 4 : x방향 - 키 떼짐.
+    # 5 : y방향 + 키가 눌림.
+    # 6 : y방향 - 키가 눌림.
+    # 7 : y방향 + 키 떼짐.
+    # 8 : y방향 - 키 떼짐.
+    # }
+    def set_state(self, keyevent):
+        if keyevent == 1 or keyevent == 5:
+            self.up = True
+        elif keyevent == 2 or keyevent == 6:
+            self.down = True
+        elif keyevent == 3 or keyevent == 7:
+            self.up = False
+        elif keyevent == 4 or keyevent == 8:
+            self.down = False
+
+    def move(self):
+        if self.idx <= 1:
+            if self.up and self.pos["y"] > 0:
+                self.pos["y"] = max(self.pos["y"] - DEFAULT_SPEED, 0)
+            elif self.down and self.pos["y"] < SCREEN_HEIGHT - PADDLE_HEIGHT:
+                self.pos["y"] = min(
+                    self.pos["y"] + DEFAULT_SPEED, SCREEN_HEIGHT - PADDLE_HEIGHT
+                )
+        else:
+            if self.down and self.pos["x"] > 0:
+                self.pos["x"] = max(self.pos["x"] - DEFAULT_SPEED, 0)
+            elif self.up and self.pos["x"] < SCREEN_WIDTH - PADDLE_WIDTH:
+                self.pos["x"] = min(
+                    self.pos["x"] + DEFAULT_SPEED, SCREEN_WIDTH - PADDLE_WIDTH
+                )
+
+    def reset_pos(self):
+        self.pos = self.initial_position()
+        self.score = 0
