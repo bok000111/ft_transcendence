e8a9af69dcfc3cae8062377c527edebe13d45cbc
mingkang <mingkang@student.42seoul.kr>
2024-06-21 06:55:31 +0000

    game 충돌 로직 변경

--- srcs/requirements/django/ws/ball.py
+++ srcs/requirements/django/ws/ball.py
@@ -1,44 +1,61 @@
+import math
 from .constants import *
 
 
 class Ball:
     def __init__(self):
         self.reset_pos()
-        self.speed_increment = 1  # 속도가 증가하는 양
-        self.max_speed = 10  # 볼의 최대 속도
+        self.speed = DEFAULT_SPEED
+        self.speed_increment = 1.1  # 속도가 증가하는 양
+        self.max_speed = 26  # 최대 속도
+        self.collision_count = 0
 
     def move(self):
         self.pos["x"] += self.vel["x"]
         self.pos["y"] += self.vel["y"]
 
-    def bounce(self, direction):
+    def bounce(self, direction, paddle_pos=None):
         if direction == "x":
-            self.vel["x"] *= -1
+            if paddle_pos is not None:
+                reflective_intersect_y = paddle_pos - self.pos["y"]
+                normalized_relative_intersect_y = reflective_intersect_y / (
+                    PADDLE_HEIGHT / 2
+                )
+                bounce_angle = normalized_relative_intersect_y * (math.pi / 4)
+                self.vel["x"] = (
+                    -1 * self.speed * math.cos(bounce_angle)
+                    if self.vel["x"] > 0
+                    else self.speed * math.cos(bounce_angle)
+                )
+                self.vel["y"] = self.speed * math.sin(bounce_angle)
+            else:
+                self.vel["x"] = -self.vel["x"]
         elif direction == "y":
-            self.vel["y"] *= -1
-        self.increase_speed()
+            if paddle_pos is not None:
+                reflective_intersect_x = paddle_pos - self.pos["x"]
+                normalized_relative_intersect_x = reflective_intersect_x / (
+                    PADDLE_HEIGHT / 2
+                )
+                bounce_angle = normalized_relative_intersect_x * (math.pi / 4)
+                self.vel["y"] = (
+                    -1 * self.speed * math.cos(bounce_angle)
+                    if self.vel["y"] > 0
+                    else self.speed * math.cos(bounce_angle)
+                )
+                self.vel["x"] = -self.speed * math.sin(bounce_angle)
+            else:
+                self.vel["y"] = -self.vel["y"]
+        self.collision_count += 1
+        if self.collision_count == 5:
+            self.increase_speed()
+            self.collision_count = 0
 
     def increase_speed(self):
         # x와 y 방향으로 속도를 증가시킨다. 단, 최대 속도를 초과하지 않도록 한다.
-        if (
-            self.vel["x"] > 0
-            and self.vel["x"] < self.max_speed
-            or self.vel["x"] < 0
-            and self.vel["x"] > -self.max_speed
-        ):
-            self.vel["x"] += (
-                self.speed_increment if self.vel["x"] > 0 else -self.speed_increment
-            )
-        if (
-            self.vel["y"] > 0
-            and self.vel["y"] < self.max_speed
-            or self.vel["y"] < 0
-            and self.vel["y"] > -self.max_speed
-        ):
-            self.vel["y"] += (
-                self.speed_increment if self.vel["y"] > 0 else -self.speed_increment
-            )
+        if self.speed < self.max_speed:
+            self.speed *= self.speed_increment
 
     def reset_pos(self):
         self.pos = {"x": SCREEN_WIDTH / 2, "y": SCREEN_HEIGHT / 2}
-        self.vel = {"x": DEFAULT_SPEED, "y": DEFAULT_SPEED}
+        self.vel = {"x": DEFAULT_SPEED_X, "y": DEFAULT_SPEED_Y}
+        self.speed = DEFAULT_SPEED
