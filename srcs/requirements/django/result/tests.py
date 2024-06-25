cf55d438327845f6859ad5b08c69a39cf6f8b720
jbok <jbok@student.32seoul.kr>
2024-06-25 07:33:27 +0000

    [FEAT] Refactor user_id_to_username method to handle non-existent users

--- srcs/requirements/django/result/tests.py
+++ srcs/requirements/django/result/tests.py
@@ -1,73 +1,128 @@
 # tests.py
+
+import asyncio
 import os
 import random
 
-from django.test import TransactionTestCase, tag
+from faker import Faker
+from django.test import TransactionTestCase, AsyncClient
 from django.urls import reverse
-from django.contrib.auth import get_user_model
 
+from ft_transcendence.tests import timer
 from result.deploy import TournamentResultManager
 from user.factories import UserFactory
+
+faker = Faker()
+
+
+def player_ids_generator(users, length=4, amount=100):
+    for _ in range(amount):
+        yield list(map(lambda user: user.pk, faker.random_elements(
+            elements=users, unique=True, length=length)))
+
+
+def tounament_result_generator(manager, users, amount=100):
+    for pids in player_ids_generator(users, 4, amount):
+        game_id = faker.random_int(1, 1000000000)
+        game_time = faker.random_int(1600000000, 1718227200)
+
+        left_win = [10, faker.random_int(0, 9)]
+        right_win = [faker.random_int(0, 9), 10]
+
+        yield manager.start_game, (game_id, game_time, pids)
+        yield manager.save_sub_game, (game_id, [2, *random.choice([left_win, right_win])])
+        yield manager.save_sub_game, (game_id, [3, *random.choice([left_win, right_win])])
+        yield manager.save_sub_game, (game_id, [1, *random.choice([left_win, right_win])])
 
 
 class TournamentResultTest(TransactionTestCase):
 
     def setUp(self):
-        self.fake_users = [UserFactory() for _ in range(4)]
-        self.tournament_contract = TournamentResultManager(
-            os.getenv("ENDPOINT"))
+        self.TEST_AMOUNT = 4
+        self.client = AsyncClient()
+        self.users = UserFactory.create_batch(self.TEST_AMOUNT)
+        asyncio.run(self.asyncSetUp())
 
-    @tag("slow")
-    def test_tournament_flow(self):
+    async def asyncSetUp(self):
+        self.manager = await TournamentResultManager.instance()
 
-        game_id = random.randint(1, 1000000000)
-        game_time = random.randint(1600000000, 1718227200)
-        player_ids = [user.pk for user in self.fake_users]
-        print(player_ids)
+        # 로컬에서 실행시 블록체인 스냅샷 생성
+        if os.getenv("HARDHAT_ENDPOINT"):
+            response = await self.manager.w3.provider.make_request("evm_snapshot", [])
+            self.snapshot_id = response["result"]
 
-        arr = [user.username for user in self.fake_users]
-        print(arr)
+    def tearDown(self):
+        # 블록체인 스냅샷 복구
+        if os.getenv("HARDHAT_ENDPOINT"):
+            asyncio.run(self.manager.w3.provider.make_request(
+                "evm_revert", [self.snapshot_id]))
+        TournamentResultManager._reset()
 
-        for i in player_ids:
-            print(get_user_model().objects.get(pk=i).username)
+    async def test_transactions(self):
+        game_id = faker.random_int(1, 1000000000)
+        game_time = faker.random_int(1600000000, 1718227200)
+        player_ids = player_ids_generator(self.users, 4, 1).__next__()
 
         left_win = [10, random.randint(0, 9)]
         right_win = [random.randint(0, 9), 10]
         random_win = [left_win, right_win]
 
-        # Start the game
-        self.tournament_contract.start_game(game_id, game_time, player_ids)
+        await self.manager.start_game(game_id, game_time, player_ids)
+        await self.manager.save_sub_game(game_id, [2] + random_win[random.randint(0, 1)])
+        await self.manager.save_sub_game(game_id, [3] + random_win[random.randint(0, 1)])
+        await self.manager.save_sub_game(game_id, [1] + random_win[random.randint(0, 1)])
 
-        # Save sub games
+        await self.manager._wait_all()
 
-        self.tournament_contract.save_sub_game(
-            game_id, [2] + random_win[random.randint(0, 1)]
-        )
-        self.tournament_contract.save_sub_game(
-            game_id, [3] + random_win[random.randint(0, 1)]
-        )
-        self.tournament_contract.save_sub_game(
-            game_id, [1] + random_win[random.randint(0, 1)]
+    async def test_result_view(self):
+        self.TEST_AMOUNT = 1
+        response = await asyncio.gather(
+            *[func(*args) for func, args in tounament_result_generator(self.manager, self.users, self.TEST_AMOUNT)]
         )
 
-        # Get all tournaments and print them
-        tournaments = self.tournament_contract.get_all_tournaments()
-        print(tournaments)
-        # for res in tournaments:
-        #     print(TournamentResult(res))
+        await self.manager._wait_all()
 
-        # Assert the tournaments data
-        self.assertTrue(len(tournaments) > 0)
+        response = await self.client.get(reverse("result"), secure=True)
 
-    @tag("slow")
-    def test_tournament_result_view(self):
+        # print(*(response.json()["data"]), sep="\n")
 
-        # Test the view
-        response = self.client.get(reverse("result"), secure=True)
-        self.assertContains(response, "", status_code=200)
+        self.assertContains(response, "success", status_code=200)
+        if os.getenv("HARDHAT_ENDPOINT"):
+            self.assertEqual(len(response.json()["data"]), self.TEST_AMOUNT)
 
-        data = response.json()
+    async def test_stress_transaction(self):
+        response = await asyncio.gather(
+            *[func(*args) for func, args in tounament_result_generator(self.manager, self.users, self.TEST_AMOUNT)]
+        )
 
-        print(data)
+        await self.manager._wait_all()
 
-        self.assertTrue(len(data) > 0)
+        response = await self.client.get(reverse("result"), secure=True)
+        self.assertContains(response, "success", status_code=200)
+        if os.getenv("HARDHAT_ENDPOINT"):
+            self.assertEqual(len(response.json()["data"]), self.TEST_AMOUNT)
+
+    async def test_caching(self):
+        response = await asyncio.gather(
+            *[func(*args) for func, args in tounament_result_generator(self.manager, self.users, self.TEST_AMOUNT)]
+        )
+
+        await self.manager._wait_all()
+
+        with timer("first request"):
+            response = await self.client.get(reverse("result"), secure=True)
+        self.assertContains(response, "success", status_code=200)
+        if os.getenv("HARDHAT_ENDPOINT"):
+            self.assertEqual(len(response.json()["data"]), self.TEST_AMOUNT)
+
+        with timer("second request"):
+            response = await self.client.get(reverse("result"), secure=True)
+        self.assertContains(response, "success", status_code=200)
+        if os.getenv("HARDHAT_ENDPOINT"):
+            self.assertEqual(len(response.json()["data"]), self.TEST_AMOUNT)
+
+        with timer("third request"):
+            response = await self.client.get(reverse("result"), secure=True)
+        self.assertContains(response, "success", status_code=200)
+        if os.getenv("HARDHAT_ENDPOINT"):
+            self.assertEqual(len(response.json()["data"]), self.TEST_AMOUNT)
