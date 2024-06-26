ba334f59bee2c12cef1cc43f21baa69bf620cc56
Sunwoo Song <sunwsong@c1r5s5.42seoul.kr>
2024-06-26 19:58:32 +0900

    [FEAT] pull from 게임-구현 ver.2

--- srcs/requirements/django/ws/ai_player.py
+++ srcs/requirements/django/ws/ai_player.py
@@ -3,8 +3,50 @@ import time
 import random
 from .constants import *
 from .player import Player
+from .ball import Ball
 
 
 class AI_Player(Player):
     def __init__(self, idx, uid, channel_name, nickname):
         super().__init__(idx, uid, channel_name, nickname)
+        self.destination = SCREEN_HEIGHT / 2
+
+    def get_destination(self, ball):
+        if ball.vel["x"] < 0:
+            return None
+        m = ball.vel["y"] / ball.vel["x"]
+        intercept = ball.pos["y"] - m * ball.pos["x"]
+        dest = (m * (SCREEN_WIDTH - PADDLE_WIDTH) +
+                intercept) % (2 * SCREEN_HEIGHT)
+        dest = int(min(dest, 2 * SCREEN_HEIGHT - dest))
+        dest = random.randint(
+            dest - PADDLE_HEIGHT, dest + PADDLE_HEIGHT)
+        if dest < PADDLE_HEIGHT:
+            dest = PADDLE_HEIGHT
+        elif dest > SCREEN_HEIGHT - PADDLE_HEIGHT:
+            dest = SCREEN_HEIGHT - PADDLE_HEIGHT
+        self.destination = dest
+
+    def set_state(self):
+        # print(f"AI destination: {self.destination}"
+        #       f"AI position: {self.pos['y']}")
+        if self.pos["y"] > self.destination:
+            print("up")
+            self.up = True
+            self.down = False
+        elif self.pos["y"] < self.destination:
+            self.up = False
+            self.down = True
+            print("down")
+        else:
+            self.up = False
+            self.down = False
+            print("stop")
+
+    def move(self):
+        super().move()
+        # print(f"status: {self.up}, {self.down}, {
+        #       self.pos['y']}, {self.destination}")
+        if (self.up and self.pos["y"] <= self.destination) or (self.down and self.pos["y"] >= self.destination):
+            self.pos["y"] = self.destination
+            self.set_state()
