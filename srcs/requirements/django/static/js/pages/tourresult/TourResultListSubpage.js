fbe35efa3b238065e3b04dba6418e6aa48626c81
Changhyeon Lee <changhyl@student.42seoul.kr>
2024-06-28 19:34:40 +0900

    [MERGE] move f.e. files from front_normal branch to main branch

--- srcs/requirements/django/static/js/pages/tourresult/TourResultListSubpage.js
+++ srcs/requirements/django/static/js/pages/tourresult/TourResultListSubpage.js
@@ -13,58 +13,70 @@ class TourResultListSubpage extends SubPage {
         `;
 
         this.$tourList = this.$elem.querySelector("#tour_list");
+
         try {
             await tourResultListAPI.request();
-            for (let i = 0; i < tourResultListAPI.recvData.data.results.length; i++) {
-                let accordionItem = document.createElement("div");
-                accordionItem.className = "accordion-item";
-                accordionItem.innerHTML = `
-                    <h2 class="accordion-header" id="flush-heading${i}">
-                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapse${i}" aria-expanded="false" aria-controls="flush-collapse${i}">
-                            ${tourResultListAPI.recvData.data.results[i].timestamp}
-                        </button>
-                    </h2>
-                    <div id="flush-collapse${i}" class="accordion-collapse collapse" aria-labelledby="flush-heading${i}" data-bs-parent="#list_example">
-                        <div class="accordion-body" id="detailTable-${i}"></div>
-                    </div>`;
-                let tableItem = this.$elem.querySelector(`#detailTable-${i}`);
-                tableItem.innerHTML = `
-                <table class="table">
-                <thead>
-                    <tr>
-                    <th scope="col">VS</th>
-                    <th scope="col">type</th>
-                    <th scope="col">winner</th>
-                    <th scope="col">score</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                    <th scope="row">${tourResultListAPI.recvData.data.results[i].sub_games[0].players[0]} vs ${tourResultListAPI.recvData.data.results[i].sub_games[0].players[1]}</th>
-                    <td>${tourResultListAPI.recvData.data.results[i].sub_games[0].game_type}</td>
-                    <td>${tourResultListAPI.recvData.data.results[i].sub_games[0].winner}</td>
-                    <td>${tourResultListAPI.recvData.data.results[i].sub_games[0].score[0]} : ${tourResultListAPI.recvData.data.results[i].sub_games[0].score[1]}</td>
-                    </tr>
-                    <tr>
-                    <th scope="row">${tourResultListAPI.recvData.data.results[i].sub_games[1].players[0]} vs ${tourResultListAPI.recvData.data.results[i].sub_games[1].players[1]}</th>
-                    <td>${tourResultListAPI.recvData.data.results[i].sub_games[1].game_type}</td>
-                    <td>${tourResultListAPI.recvData.data.results[i].sub_games[1].winner}</td>
-                    <td>${tourResultListAPI.recvData.data.results[i].sub_games[1].score[0]} : ${tourResultListAPI.recvData.data.results[i].sub_games[1].score[1]}</td>
-                    </tr>
-                    <tr>
-                    <th scope="row">${tourResultListAPI.recvData.data.results[i].sub_games[2].players[0]} vs ${tourResultListAPI.recvData.data.results[i].sub_games[2].players[1]}</th>
-                    <td>${tourResultListAPI.recvData.data.results[i].sub_games[2].game_type}</td>
-                    <td>${tourResultListAPI.recvData.data.results[i].sub_games[2].winner}</td>
-                    <td>${tourResultListAPI.recvData.data.results[i].sub_games[2].score[0]} : ${tourResultListAPI.recvData.data.results[i].sub_games[2].score[1]}</td>
-                    </tr>
-                </tbody>
-                </table>`;
-                this.$tourList.appendChild(accordionItem);
+            
+            if (tourResultListAPI.recvData && tourResultListAPI.recvData.data) {
+                this.drawItems();
+            } else {
+                throw new Error("No data received");
             }
         }
         catch {
             alert("Failed to load Tournament Result...");
             this.route("main_page/main_subpage");
+        }
+    }
+
+    drawItems() {
+        for (let i = 0; i < tourResultListAPI.recvData.data.length; i++) {
+            let accordionItem = document.createElement("div");
+            accordionItem.className = "accordion-item";
+            accordionItem.innerHTML = `
+                <h2 class="accordion-header" id="flush-heading${i}">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapse${i}" aria-expanded="false" aria-controls="flush-collapse${i}">
+                        ${tourResultListAPI.recvData.data[i].timestamp}
+                    </button>
+                </h2>
+                <div id="flush-collapse${i}" class="accordion-collapse collapse" aria-labelledby="flush-heading${i}" data-bs-parent="#list_example">
+                    <div class="accordion-body" id="detailTable-${i}"></div>
+                </div>`;
+            
+            this.$tourList.appendChild(accordionItem);
+
+            let tableItem = document.querySelector(`#detailTable-${i}`);
+            tableItem.innerHTML = `
+            <table class="table">
+            <thead>
+                <tr>
+                <th scope="col">VS</th>
+                <th scope="col">type</th>
+                <th scope="col">winner</th>
+                <th scope="col">score</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                <th scope="row">${tourResultListAPI.recvData.data[i].sub_games[0].players[0]} vs ${tourResultListAPI.recvData.data[i].sub_games[0].players[1]}</th>
+                <td>${tourResultListAPI.recvData.data[i].sub_games[0].game_type}</td>
+                <td>${tourResultListAPI.recvData.data[i].sub_games[0].winner}</td>
+                <td>${tourResultListAPI.recvData.data[i].sub_games[0].score[0]} : ${tourResultListAPI.recvData.data[i].sub_games[0].score[1]}</td>
+                </tr>
+                <tr>
+                <th scope="row">${tourResultListAPI.recvData.data[i].sub_games[1].players[0]} vs ${tourResultListAPI.recvData.data[i].sub_games[1].players[1]}</th>
+                <td>${tourResultListAPI.recvData.data[i].sub_games[1].game_type}</td>
+                <td>${tourResultListAPI.recvData.data[i].sub_games[1].winner}</td>
+                <td>${tourResultListAPI.recvData.data[i].sub_games[1].score[0]} : ${tourResultListAPI.recvData.data[i].sub_games[1].score[1]}</td>
+                </tr>
+                <tr>
+                <th scope="row">${tourResultListAPI.recvData.data[i].sub_games[2].players[0]} vs ${tourResultListAPI.recvData.data[i].sub_games[2].players[1]}</th>
+                <td>${tourResultListAPI.recvData.data[i].sub_games[2].game_type}</td>
+                <td>${tourResultListAPI.recvData.data[i].sub_games[2].winner}</td>
+                <td>${tourResultListAPI.recvData.data[i].sub_games[2].score[0]} : ${tourResultListAPI.recvData.data[i].sub_games[2].score[1]}</td>
+                </tr>
+            </tbody>
+            </table>`;
         }
     }
 
